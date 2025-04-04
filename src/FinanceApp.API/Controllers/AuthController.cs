fc2f3c45f3dcaf209ab0d9a6ef2a0c2cfb5c4f61
mario-alexx <marioatoroh@gmail.com>
2025-04-04 16:49:16 -0300

    updating endpoint route in authController

--- src/FinanceApp.API/Controllers/AuthController.cs
+++ src/FinanceApp.API/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceApp.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
