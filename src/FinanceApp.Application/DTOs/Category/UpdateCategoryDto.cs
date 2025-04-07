69373605ec2d4e5f778a10a6bda53df3519544e5
mario-alexx <marioatoroh@gmail.com>
2025-04-07 17:49:14 -0300

    update DTOs: removing DataAnnotations

--- src/FinanceApp.Application/DTOs/Category/UpdateCategoryDto.cs
+++ src/FinanceApp.Application/DTOs/Category/UpdateCategoryDto.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using FinanceApp.Domain.Enums;
 
 namespace FinanceApp.Application.DTOs.Category;
@@ -7,13 +6,9 @@ namespace FinanceApp.Application.DTOs.Category;
 
 public class UpdateCategoryDto
 {
-    [Required]
     public Guid Id { get; set; }
 
-    [Required]
-    [MaxLength(100)]
     public string Name { get; set; } = null!;
 
-    [Required]
     public TransactionType Type { get; set; }
 }
--- src/FinanceApp.Application/DTOs/Transaction/UpdateTransactionDto.cs
+++ src/FinanceApp.Application/DTOs/Transaction/UpdateTransactionDto.cs
@@ -6,19 +6,14 @@ namespace FinanceApp.Application.DTOs.Transaction;
 
 public class UpdateTransactionDto
 {
-    [Required]
     public Guid Id { get; set; }
 
-    [Required]
     public decimal Amount { get; set; }
 
-    [Required]
     public TransactionType Type { get; set; }
 
-    [Required]
     public DateTime Date { get; set; }
 
-    [Required]
     public Guid CategoryId { get; set; }
 
     public string Description { get; set; } = string.Empty;
